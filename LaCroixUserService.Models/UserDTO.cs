24f0a47f3ea113bc32db946820a87479695d9233
purpeliz <purple.lizard0102@gmail.com>
2026-02-12 23:40:03 +0300

    added UserDTO

---
+++ LaCroixUserService.Models/UserDTO.cs
@@ -0,0 +1,6 @@
+﻿using LaCroixUserService.Contracts.Enums;
+
+namespace LaCroixUserService.Models
+{
+    public record class UserDTO(string UserName, string Email, string Name, Gender Gender, DateTime? Birthday);
+}
