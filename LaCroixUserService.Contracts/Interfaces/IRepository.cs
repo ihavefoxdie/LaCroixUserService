ec475b1bd6306becc4085a9d05f4b6e991bddada
purpeliz <purple.lizard0102@gmail.com>
2026-02-12 23:40:24 +0300

    added IRepository interface

---
+++ LaCroixUserService.Contracts/Interfaces/IRepository.cs
@@ -0,0 +1,10 @@
+﻿namespace LaCroixUserService.Contracts.Interfaces;
+
+public interface IRepository<T>
+{
+    Task<T?> Add(T entity);
+    Task<T?> GetById(int id);
+    Task<IEnumerable<T>> GetAll();
+    Task<T?> Update(T entity);
+    Task Delete(int id);
+}
