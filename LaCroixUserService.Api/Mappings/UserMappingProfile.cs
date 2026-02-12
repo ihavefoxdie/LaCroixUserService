a6c5cec8fbfef4aa7e4ee6fc571e4c715d783e0c
purpeliz <purple.lizard0102@gmail.com>
2026-02-12 23:48:02 +0300

    Added AutoMapper and a UserDTO/User mapping

---
+++ LaCroixUserService.Api/Mappings/UserMappingProfile.cs
@@ -0,0 +1,18 @@
+﻿using AutoMapper;
+using LaCroixUserService.Api.Entities;
+using LaCroixUserService.Models;
+
+namespace LaCroixUserService.Api.Mappings;
+
+public class UserMappingProfile : Profile
+{
+    public UserMappingProfile()
+    {
+        CreateMap<User, UserDTO>()
+            .ForMember(dest => dest.UserName, opt => opt.MapFrom(src => src.Username))
+            .ForMember(dest => dest.Email, opt => opt.MapFrom(src => src.Email))
+            .ForMember(dest => dest.Name, opt => opt.MapFrom(src => src.Name))
+            .ForMember(dest => dest.Birthday, opt => opt.MapFrom(src => src.Birthday))
+            .ForMember(dest => dest.Gender, opt => opt.MapFrom(src => src.Gender));
+    }
+}
