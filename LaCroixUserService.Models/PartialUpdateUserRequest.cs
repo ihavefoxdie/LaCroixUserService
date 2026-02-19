6d1a10e7573bad961a14dd05b1749224500d625f
purpeliz <purple.lizard0102@gmail.com>
2026-02-19 17:25:25 +0300

    chore(dto): add new DTOs for user requests
    - Add CreateUserRequest for POST requests
    - Add PartialUpdateUserRequest for PATCH requests
    - Add UpdateUserRequest for PUT requests

---
+++ LaCroixUserService.Models/PartialUpdateUserRequest.cs
@@ -0,0 +1,25 @@
+﻿using LaCroix.UserService.Contracts.Enums;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace LaCroix.UserService.Models
+{
+    public class PartialUpdateUserRequest
+    {
+        public string? Username { get; set; }
+
+        [EmailAddress]
+        public string? Email { get; set; }
+
+        public string? Name { get; set; }
+
+        [MinLength(8)]
+        public string? Password { get; set; }
+
+        public Gender? Gender { get; set; }
+
+        public DateTime? Birthday { get; set; }
+    }
+}
---
+++ LaCroixUserService.Models/UpdateUserRequest.cs
@@ -0,0 +1,26 @@
+﻿using LaCroix.UserService.Contracts.Enums;
+using System.ComponentModel.DataAnnotations;
+
+namespace LaCroix.UserService.Models
+{
+    public class UpdateUserRequest
+    {
+        [Required]
+        public required int Id { get; set; }
+
+        [Required]
+        public required string Username { get; set; }
+
+        [Required, EmailAddress]
+        public required string Email {  get; set; }
+
+        public string Name { get; set; } = string.Empty;
+
+        [Required, MinLength(8)]
+        public required string Password { get; set; }
+
+        public Gender Gender { get; set; } = Gender.Unknown;
+
+        public DateTime? Birthday { get; set; }
+    }
+}
