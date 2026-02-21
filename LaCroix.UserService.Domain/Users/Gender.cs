a9cb58d4614c041ff7e4a0d6d36d95d484adce99
purpeliz <purple.lizard0102@gmail.com>
2026-02-21 17:12:19 +0300

    refactor: add User entity and its related classes/enums to the Domain layer
    - add new Email class
    - use Email class in the User entity

---
+++ LaCroix.UserService.Domain/Users/Gender.cs
@@ -0,0 +1,8 @@
+﻿namespace LaCroix.UserService.Domain.Users;
+
+public enum Gender
+{
+    Male = 1,
+    Female = 2,
+    Unknown = 3
+}
---
+++ LaCroix.UserService.Domain/Users/User.cs
@@ -0,0 +1,72 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace LaCroix.UserService.Domain.Users;
+
+/// <summary>
+/// Represents a user
+/// </summary>
+public class User
+{
+    public User(string nickname, string email, string passwordHash, string firstName, string lastName, Gender gender, DateTime? birthday, Status status = Status.Active, UserRole role = UserRole.User)
+    {
+        Id = new Guid();
+        Nickname = nickname;
+        Email = new (email);
+        PasswordHash = passwordHash;
+        FirstName = firstName;
+        LastName = lastName;
+        Gender = gender;
+        Birthday = birthday;
+        Status = status;
+        Role = role;
+    }
+
+
+    /// <summary>
+    /// User's ID.
+    /// </summary>
+    [Key]
+    public Guid Id { get; init; }
+    /// <summary>
+    /// User's login.
+    /// </summary>
+    public string Nickname { get; private set; }
+    /// <summary>
+    /// User's email.
+    /// </summary>
+    [EmailAddress]
+    public Email Email { get; private set; }
+    /// <summary>
+    /// Hash of the user's password.
+    /// </summary>
+    public string PasswordHash { get; private set; }
+
+    /// <summary>
+    /// User's name.
+    /// </summary>
+    public string FirstName { get; set; } = string.Empty;
+    /// <summary>
+    /// User's surname.
+    /// </summary>
+    public string LastName { get; set; } = string.Empty;
+    /// <summary>
+    /// User's gender.
+    /// </summary>
+    public Gender Gender { get; set; }
+    /// <summary>
+    /// User's date of birth.
+    /// </summary>
+    public DateTime? Birthday { get; set; }
+
+    /// <summary>
+    /// Date of the user's creation.
+    /// </summary>
+    public DateTime CreatedDate { get; init; } = DateTime.Now;
+    /// <summary>
+    /// Date of the last modification made to this record.
+    /// </summary>
+    public DateTime? UpdatedDate { get; private set; }
+
+    public Status Status { get; private set; }
+    public UserRole Role { get; private set; }
+}
