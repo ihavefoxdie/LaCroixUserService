d2cfdc59693c90c8b2ffb1e8cc0f41987cdf1a15
purpeliz <purple.lizard0102@gmail.com>
2026-02-21 17:15:10 +0300

    refactor: introduce IUserRepository in domain layer

---
+++ LaCroix.UserService.Domain/Contracts/IUserRepository.cs
@@ -0,0 +1,14 @@
+﻿using LaCroix.UserService.Domain.Users;
+
+namespace LaCroix.UserService.Domain.Contracts;
+
+public interface IUserRepository
+{
+    public Task AddAsync(User entity, CancellationToken cancellationToken);
+
+    public Task<User?> GetByIdAsync(Guid id, CancellationToken cancellationToken);
+
+    public Task<User?> GetByEmailAsync(string email, CancellationToken cancellationToken);
+
+    public void Deactivate(User user);
+}
