9a2859dc3e931e58618ea7f0252ee52f9bacdb38
purpeliz <purple.lizard0102@gmail.com>
2026-02-23 17:23:06 +0300

    feat(application): add user login/register requests and gender parser
    - add UserLoginRequest
    - add UserRegisterRequest
    - add GenderParser

---
+++ LaCroix.UserService.Application/UseCases/UserLoginRequest.cs
@@ -0,0 +1,5 @@
+﻿using LaCroix.UserService.Application.Contracts;
+
+namespace LaCroix.UserService.Application.UseCases;
+
+public record UserLoginRequest(string Email, string Password) : IRequest<Guid>;
---
+++ LaCroix.UserService.Application/UseCases/UserRegisterRequest.cs
@@ -0,0 +1,13 @@
+﻿using LaCroix.UserService.Application.Contracts;
+
+namespace LaCroix.UserService.Application.UseCases;
+
+public record UserRegisterRequest(
+    string Nickname,
+    string Email,
+    string Password,
+    string? FirstName,
+    string? LastName,
+    string? Gender,
+    DateTime? Birthday
+) : IRequest<Guid>;
