2ddcfbc6908f4916c5078aedde3b03316b7c1952
purpeliz <purple.lizard0102@gmail.com>
2026-02-24 17:34:30 +0300

    fixes(mediator): fix IMediator interface

--- LaCroix.UserService.Application/Contracts/IMediator.cs
+++ LaCroix.UserService.Application/Contracts/IMediator.cs
@@ -2,6 +2,14 @@
 
 public interface IMediator
 {
-    Task<T1> SendAsync<T1>(IRequest<T1> request, CancellationToken cancellationToken = default);
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <typeparam name="T1">Request type.</typeparam>
+    /// <typeparam name="T2">Response type.</typeparam>
+    /// <param name="request"></param>
+    /// <param name="cancellationToken"></param>
+    /// <returns></returns>
+    Task<T2> SendAsync<T1, T2>(T1 request, CancellationToken cancellationToken = default) where T1 : IRequest<T2>;
 }
 
