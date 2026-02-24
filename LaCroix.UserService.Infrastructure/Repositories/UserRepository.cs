50f7babb2789e2f2ef8ed72b373853be57521971
purpeliz <purple.lizard0102@gmail.com>
2026-02-24 17:37:54 +0300

    feat(infrastructure): implement user repository

---
+++ LaCroix.UserService.Infrastructure/Repositories/UserRepository.cs
@@ -0,0 +1,31 @@
+﻿using LaCroix.UserService.Domain.Contracts;
+using LaCroix.UserService.Domain.Users;
+using LaCroix.UserService.Infrastructure.Data;
+using Microsoft.EntityFrameworkCore;
+
+namespace LaCroix.UserService.Infrastructure.Repositories;
+
+public class UserRepository : IUserRepository
+{
+    private readonly UserDbContext _dbContext;
+
+    public UserRepository(UserDbContext userDbContext)
+    {
+        _dbContext = userDbContext;
+    }
+
+    public async Task AddAsync(User entity, CancellationToken cancellationToken)
+    {
+        await _dbContext.Users.AddAsync(entity, cancellationToken);
+    }
+
+    public async Task<User?> GetByEmailAsync(Email email, CancellationToken cancellationToken)
+    {
+        return await _dbContext.Users.FirstOrDefaultAsync(x => x.Email == email, cancellationToken);
+    }
+
+    public async Task<User?> GetByIdAsync(Guid id, CancellationToken cancellationToken)
+    {
+        return await _dbContext.Users.FirstOrDefaultAsync(x => x.Id == id, cancellationToken);
+    }
+}
